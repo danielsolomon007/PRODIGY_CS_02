bdf143e9c73bbee5f10904a75ac96ff8f2f65356
danielsolomon007 <danielsolomonofficialacc@gmail.com>
2025-07-01 22:34:41 +0530

    task02

---
+++ test02.py
@@ -0,0 +1,59 @@
+from PIL import Image
+
+def encrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+
+    width, height = img.size
+
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+
+            # swapping red and blue channels
+            encrypted_pixel = (b, g, r)
+
+            pixels[i, j] = encrypted_pixel
+
+    img.save(output_path)
+    print("Image encrypted successfully!")
+
+def decrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+
+    width, height = img.size
+
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+
+
+
+            # swapping red and blue channel
+            decrypted_pixel = (b, g, r)
+
+
+            pixels[i, j] = decrypted_pixel
+
+
+    img.save(output_path)
+    print("Image decrypted successfully!")
+
+ 
+ # image path
+
+input_image = r"C:\Users\jdani\Desktop\Task-02\input.jpg"
+encrypted_image = r"C:\Users\jdani\Desktop\Task-02\encrypted_image.jpg"
+decrypted_image = r"C:\Users\jdani\Desktop\Task-02\decrypted_image.jpg"
+
+
+# Encrypt the image
+
+encrypt_image(input_image, encrypted_image, key=None)
+
+# Decrypt the image
+
+decrypt_image(encrypted_image, decrypted_image, key=None)
